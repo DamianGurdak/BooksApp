f828a049db0d4d6ad320d138685fa3b536af4139
Damian Gurdak <damiangurdakk@gmail.com>
2022-08-15 02:31:40 +0200

    improving project

--- src/js/script.js
+++ src/js/script.js
@@ -15,13 +15,22 @@ const select = {
   },
 };
 
-//referencje
 const templates = {
   menuProduct: Handlebars.compile(
     //referrencja do szablonu
     document.querySelector(select.tamplateOf.menuProduct).innerHTML
   ),
   // robić referencje do listy .books-list.?
+};
+
+const backgroundColor = {
+  colors: {
+    colorOne: 'background: linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);', // Rating < 6
+    colorTwo: 'background: linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);', // Rating > 6 && <= 8
+    colorThree: 'background: linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);', // Rating > 8 && <= 9
+    colorFour: 'background: linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);', // Rating > 9
+  },
+
 };
 
 const favouriteBooks = [];
@@ -33,8 +42,8 @@ class Book {
     const thisBook = this;
     console.log('thisBook:', thisBook);
 
-    thisBook.id = id; // ?
-    thisBook.data = data; // ?
+    thisBook.id = id;
+    thisBook.data = data;
     thisBook.renderInMenu();
     thisBook.getElements(); // getElement moze być pierwsze wywołane? nie bo bierze elementy z render menu
     thisBook.initActions(); // ma być uruchamiana po funkcji render
@@ -45,9 +54,6 @@ class Book {
 
     thisBook.dom = {};
     thisBook.dom.image = thisBook.element.querySelector(select.book.image);
-    // thisBook.dom.filters = document.querySelector(
-    //   select.menuProduct.filters
-    // );
   }
 
   renderInMenu() {
@@ -77,37 +83,30 @@ class Book {
     });
   }
 
-  // toggleFavorite() {
-  //   const thisBook = this;
-
-  //   for (let checkbox of everyCheckbox) {
-  //     if (
-  //       checkbox.tagName === 'INPUT' &&
-  //       checkbox.type === 'checkbox' &&
-  //       checkbox.name === 'filter'
-  //     ) {
-  //       console.log();
-  //     }
-  //   }
-  // }
-
   initFavoriteBooks(bookId) {
     // kiedy dajemy a kiedy nie dajemy argumnet w funkcji/metodzie
     const thisBook = this;
 
     const index = favouriteBooks.indexOf(bookId); // argument book czym on jest skąd wiemy ze jest to pojeduncza ksiązak skoro nie ma tego zdefiniowanego
     //czy to book a app z funkcji init menu czy to tosamo?
-    //w poradniku jesr argument w cudzysłwoie a tu nie ??
+    // console.log('index:', index);
 
     if (!favouriteBooks.includes(bookId)) {
-      favouriteBooks.push(bookId); //zapisać id książki w tablicy wiev book to nie book ale id to przeciez thisbook id // dlaczego book sie dodaje i usówa a book tylko dodaje?
+      favouriteBooks.push(bookId); //zapisać id książki w tablicy
       thisBook.dom.image.classList.add('favorite'); // dodać klasę favorite
     } else if (index > -1) {
-      favouriteBooks.splice(index, 1);
+      favouriteBooks.splice(index, 1); //usuwaa id takiej książki z tablicy favoriteBooks
       thisBook.dom.image.classList.remove('favorite');
     }
     console.log('Favorite books: ', favouriteBooks);
   }
+
+  determineRatingBgc(rating) {
+    if(rating < 6) {
+     
+    };
+  };
+
 }
 
 const app = {
@@ -130,6 +129,8 @@ const app = {
     for (const book of thisApp.data.books) {
       console.log(book);
       new Book(book.id, book);
+
+      const ratingBgc = 
     }
   },
 
@@ -147,10 +148,45 @@ const app = {
         checkbox.name === 'filter'
       ) {
         console.log(checkbox.value);
+      }
 
+      const indexOf = thisApp.filters.indexOf(checkbox.value);
+
+      if (checkbox === -1) {
+        thisApp.filters.push(checkbox.value);
+        thisApp.filterBooks();
+        console.log(thisApp.filters);
+      } else {
+        thisApp.filters.splice(indexOf, 1);
+        thisApp.filterBooks();
+        console.log(thisApp.filters);
       }
     });
   },
+
+  // filterBooks: function () {
+  //   const thisApp = this;
+
+    
+
+  //   for (let book of dataSource.books) {
+
+  //     let shouldBeHidden = false;
+
+
+  //     for(const filter of filters) {
+  //       if(!book.details[filter]) {
+  //         shouldBeHidden = true;
+  //         break;
+  //       };
+
+  //         if(shouldBeHidden === true) {
+
+  //         };
+        
+  //     };
+  //   }
+  // },
 
   init: function () {
     const thisApp = this;
