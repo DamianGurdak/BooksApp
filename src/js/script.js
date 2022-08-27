4e7853f710aa889081116ec8601cf7e3561209e6
Damian Gurdak <damiangurdakk@gmail.com>
2022-08-28 01:10:03 +0200

    improve project

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,19 @@
 /* eslint-disable */ /* global Handlebars, utils */
 
 const select = {
-  tamplateOf: {
-    menuProduct: '#template-book',
+  templateOf: {
+    book: '#template-book',
+    coverImage: '.book__image', //cover --> okładka
   },
   containerOf: {
-    menu: '.books-list',
+    booksPanel: '.books-list',
   },
-  book: {
-    image: '.book__image',
-  },
+  // book: {
+  //   image: '.book__image',
+  // },
   menuProduct: {
     filters: '.filters',
   },
-};
-
-const templates = {
-  menuProduct: Handlebars.compile(
-    //referrencja do szablonu
-    document.querySelector(select.tamplateOf.menuProduct).innerHTML
-  ),
-  // robić referencje do listy .books-list.?
 };
 
 const backgroundColor = {
@@ -36,154 +29,107 @@ const backgroundColor = {
   },
 };
 
-const favouriteBooks = [];
-const filters = [];
+const favoriteBooks = []; //tablica z identyfikatorami książek, które dodano do ulubionych
+// const filters = [];
 
-class Book {
+class BookList {
   constructor() {
-    const thisBook = this;
-    console.log('thisBook:', thisBook);
+    const thisBookList = this;
+    // console.log('thisBookListList:', thisBookList);
 
-    thisBook.data = dataSource;
-    thisBook.renderInMenu();
-    thisBook.getElements();
-    thisBook.initActions(); 
+    thisBookList.data = dataSource; // jak to działa
+    thisBookList.renderBooks();
+    thisBookList.getElements();
+    thisBookList.initActions();
+  }
+
+  initData() {
+    /*
+    // dlaczego tutaj tak nie mozebyć
+
+    const thisData = this
+    thisData.data = dataSource.books;
+    */
+
+    this.data = dataSource.books;
   }
 
   getElements() {
-    const thisBook = this;
+    const thisBookList = this;
 
-    thisBook.dom = {};
-    thisBook.dom.image = thisBook.element.querySelector(select.book.image);
-    thisApp.filters = document.querySelector(select.menuProduct.filters); // z app
-    //lista ksiazek
-    thisBook.dom.bookList = thisBook.element.querySelector(select.containerOf.menu);
+    thisBookList.dom = {};
+
+    // thisBookList.dom.image = thisBookList.element.querySelector(
+    //   select.book.image
+    // );
+
+    // thisApp.filters = document.querySelector(select.menuProduct.filters); // z app
+    // // lista ksiazek
   }
 
-  renderInMenu() {
-    const thisBook = this;
-    console.log('newBook:', thisBook);
+  renderBooks() {
+    const thisBookList = this;
+    // console.log('newBook:', thisBookList);
 
-    const menuContainer = document.querySelector(select.containerOf.menu);
-    console.log(menuContainer);
+    //referencja do listy .book-list
+    const BookList = document.querySelector(select.containerOf.booksPanel);
+    // console.log(BookList);
 
-    for (const book of thisBook.data.books) {
-      //wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce. // data to konkretna ksiązka?
+    //referrencja do szablonu
+    const templates = {
+      menuProduct: Handlebars.compile(
+        document.querySelector(select.templateOf.book).innerHTML
+      ),
+    };
+
+    for (const book of thisBookList.data.books) {
+      /* this.data z initData nie działa jak sie do tego odnosze */
+
+      //wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce.
       const generatedHtml = templates.menuProduct(book);
-      // console.log(generatedHtml);
-      console.log(thisBook.data);
+      // console.log(generatedHtml); //li pojednynczych ksiązek ale  w html
 
       //Na postawie tego kodu HTML wygeneruj element DOM
       const element = utils.createDOMFromHTML(generatedHtml);
-      // console.log(element);
+      // console.log(element); //li pojednynczych ksiązek ale aktywne linki
 
       //Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
-      menuContainer.appendChild(element);
-      console.log(book);
+      BookList.appendChild(element);
+      // console.log(book);
     }
+
+    /* co pokazuje console.log, to jest chyba z constructora a wskazujena initData */
+    // console.log(thisBookList.data);
   }
 
   initActions() {
-    const thisBook = this;
+    const thisBookList = this;
 
-    thisBook.dom.image.addEventListener('dblclick', function (event) {
-      event.preventDefault();
-      thisBook.initFavoriteBooks(thisBook.id);
-    });
-    //petla po obrazkah w wraperze
-    // for() {};
-  }
+    // referencję do listy wszystkich elementów .book__image w liście .booksList
+    thisBookList.dom.CoverImage = thisBookList.element.querySelector(
+      select.templateOf.coverImage
+    );
 
-  initFavoriteBooks(bookId) {
-    // kiedy dajemy a kiedy nie dajemy argumnet w funkcji/metodzie
-    const thisBook = this;
-
-    const index = favouriteBooks.indexOf(bookId); // argument book czym on jest skąd wiemy ze jest to pojeduncza ksiązak skoro nie ma tego zdefiniowanego
-    //czy to book a app z funkcji init menu czy to tosamo?
-    // console.log('index:', index);
-
-    if (!favouriteBooks.includes(bookId)) {
-      favouriteBooks.push(bookId); //zapisać id książki w tablicy
-      thisBook.dom.image.classList.add('favorite'); // dodać klasę favorite
-    } else if (index > -1) {
-      favouriteBooks.splice(index, 1); //usuwaa id takiej książki z tablicy favoriteBooks
-      thisBook.dom.image.classList.remove('favorite');
-    }
-    console.log('Favorite books: ', favouriteBooks);
-  }
-
-  determineRatingBgc(rating) {
-    if (rating < 6) {
+    for (const book of BookList) {
+      thisBookList.dom.CoverImage.addEventListener(
+        'dbclcick',
+        function (event) {
+          event.preventDefault(); //zatrzyma domyślne zachowanie przeglądarki
+          thisBookList.dom.CoverImage.classList.add('favorite');
+          const bookId = thisBookList.dom.CoverImage.getElementById('data-id');
+          // console('bokkId: ', bookId);
+          favoriteBooks.push(bookId); //zapisuje  id książki w tablicy
+        }
+      );
     }
   }
+
+  // filerBooks() {}
+
+  // determineRatingBgc(rating) {
+  //   if (rating < 6) {
+  //   }
+  // }
 }
 
-const app = new Book();
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// const app = {
- 
-
-// initActions: function () {
-
-//     const thisApp = this;
-
-//     thisApp.filters.addEventListener('click', function (event) {
-//       event.preventDefault();
-//       // thisBook.toggleFavorite();
-//       const checkbox = event.target;
-//       if (
-//         checkbox.tagName === 'INPUT' &&
-//         checkbox.type === 'checkbox' &&
-//         checkbox.name === 'filter'
-//       ) {
-//         console.log(checkbox.value);
-//       }
-
-//       const indexOf = thisApp.filters.indexOf(checkbox.value);
-
-//       if (checkbox === -1) {
-//         thisApp.filters.push(checkbox.value);
-//         thisApp.filterBooks();
-//         console.log(thisApp.filters);
-//       } else {
-//         thisApp.filters.splice(indexOf, 1);
-//         thisApp.filterBooks();
-//         console.log(thisApp.filters);
-//       }
-//     });
-//   },
-
-//   // filterBooks: function () {
-//   //   const thisApp = this;
-
-//   //   for (let book of dataSource.books) {
-
-//   //     let shouldBeHidden = false;
-
-//   //     for(const filter of filters) {
-//   //       if(!book.details[filter]) {
-//   //         shouldBeHidden = true;
-//   //         break;
-//   //       };
-
-//   //         if(shouldBeHidden === true) {
-
-//   //         };
-
-//   //     };
-//   //   }
-//   // },
-
-//   init: function () {
-//     const thisApp = this;
-
-//     thisApp.initData();
-//     thisApp.initMenu();
-//     thisApp.getElements();
-//     thisApp.initActions();
-//   },
-// };
-
-// app.init();
+const app = new BookList(); // instncja klasy
